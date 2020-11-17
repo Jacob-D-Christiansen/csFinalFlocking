c175f084d0031eee57e538e4426a3a8fc33dc99c
Jacob <Jacob.D.Christiansen@gmail.com>
2020-11-17 16:54:03 -0700

    mozilla tutorial and start of flocking

---
+++ flocking.js
@@ -0,0 +1,15 @@
+
+
+function main(){
+    var canvas = document.querySelector("canvas");
+    var gl = canvas.getContext("webgl");
+    
+    if(gl === null){
+        alert("could not load webgl");
+    }
+    
+    // make trianlgle object to hold info
+    // make array of triangle vertexes
+    // make shaders
+    // animate
+}
